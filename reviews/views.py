98bb66c9084450876d54f4b9fc9a8975e7e35a54
WhiteJohny <vvvfrog2004@gmail.com>
2023-10-03 19:57:35 +0300

    2 funcs - index and searching

--- reviews/views.py
+++ reviews/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
 
-# Create your views here.
+def index(request):
+    return render(request, "base.html")
+def searching(request):
+    search = request.GET.get('search')
+    return render(request, "search.html", {"search": search})
